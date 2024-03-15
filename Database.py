0dbdb926ff6bcef7a67fb551c8442d24cc367756
domenic <domenic.campana@gmail.com>
2024-03-15 11:42:39 -0400

    db

--- Database.py
+++ Database.py
@@ -5,13 +5,23 @@ connection = sqlite3.connect("fsae.db")
 cursor = connection.cursor()
 #cursor.execute("CREATE TABLE fsae_database (Date TEXT, Time TEXT, Parameter TEXT, Value INTEGER )")
 
-cursor.execute("INSERT INTO fsae_database VALUES ('02/10/2024','8:37', 'Speed', 200)")
-cursor.execute("INSERT INTO fsae_database VALUES ('02/10/2024','8:37', 'Motor Temp', 20)")
-cursor.execute("INSERT INTO fsae_database VALUES ('02/10/2024','8:37', 'Bat Temp', 20)")
-cursor.execute("INSERT INTO fsae_database VALUES ('02/10/2024','8:37', 'Battery %', 1)")
-cursor.execute("INSERT INTO fsae_database VALUES ('02/10/2024','8:37', 'Power', 120)")
-cursor.execute("INSERT INTO fsae_database VALUES ('02/10/2024','8:37', 'Lap Time', 10)")
-cursor.execute("INSERT INTO fsae_database VALUES ('02/10/2024','8:37', 'Lap Power', 150)") #Hi
-cursor.close()
+table_schema = '''
+    CREATE TABLE IF NOT EXISTS telemetry_data (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Speed REAL,
+        MotorTemperature REAL,
+        CoolantTemperature REAL,
+        CoolantFlowRate REAL,
+        MotorCurrent REAL,
+        BatteryVoltage REAL,
+        BatteryTemperature REAL,
+        LVBatteryVoltage REAL,
+        GPSLocation TEXT,
+        Acceleration3Axis TEXT,
+        Direction3Axis TEXT,
+        LapTime REAL
+    );
+'''
+cursor.execute(table_schema)
 
 
