dc2b44ef9e8bd12f57ec51f28aedcb0d78402cf3
domenic <domenic.campana@gmail.com>
2024-04-01 15:49:53 -0400

    updated code

--- Database.py
+++ Database.py
@@ -1,4 +1,6 @@
 import sqlite3
+import rando
+import time
 
 connection = sqlite3.connect("fsae.db")
 
@@ -24,4 +26,46 @@ table_schema = '''
 '''
 cursor.execute(table_schema)
 
+# Function to generate simulated GPS data
+def generate_gps_data():
+    latitude = random.uniform(-90, 90)
+    longitude = random.uniform(-180, 180)
+    return "Latitude: {}, Longitude: {}".format(latitude, longitude)
 
+# Function to generate simulated acceleration data
+def generate_acceleration_data():
+    acceleration_x = random.uniform(-5, 5)
+    acceleration_y = random.uniform(-5, 5)
+    acceleration_z = random.uniform(-5, 5)
+    return "X: {}, Y: {}, Z: {}".format(acceleration_x, acceleration_y, acceleration_z)
+
+# Function to generate simulated magnetic field data
+def generate_magnetic_field_data():
+    magnetic_x = random.uniform(-1, 1)
+    magnetic_y = random.uniform(-1, 1)
+    magnetic_z = random.uniform(-1, 1)
+    return "X: {}, Y: {}, Z: {}".format(magnetic_x, magnetic_y, magnetic_z)
+
+# Main loop to read sensor data and insert into database
+while True:
+    # Simulate sensor readings
+    gps_data = generate_gps_data()
+    acceleration_data = generate_acceleration_data()
+    magnetic_field_data = generate_magnetic_field_data()
+
+    # Insert data into the database
+    cursor.execute('''INSERT INTO telemetry_data 
+                    (GPSLocation, Acceleration3Axis, Direction3Axis) 
+                    VALUES (?, ?, ?)''', (gps_data, acceleration_data, magnetic_field_data))
+    
+    # Commit the transaction
+    connection.commit()
+
+    # Print confirmation message
+    print("Data inserted successfully.")
+
+    # Delay between readings (adjust as needed)
+    time.sleep(1)
+
+# Close connection when done (This part will not be reached in an infinite loop)
+connection.close()
